25101d1749acdcb4044b72ed1a41a56585d78b90
Ariel Esteves <laydro68@gmail.com>
2022-07-23 20:57:37 -0300

    First Commit

---
+++ src/test/java/com/soften/ariel/ArielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soften.ariel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
